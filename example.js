e5239db812d19c52239fd92de9baac99a03860d3
ak9250 <ahsen.khaliq@gmail.com>
2017-03-31 19:15:27 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 const WhaleClub = require('whaleclub');
 
-const API_KEY = '8a1c5feb-9b4f-43b8-8a2c-d7d173e23929';
-const wc = new WhaleClub("8a1c5feb-9b4f-43b8-8a2c-d7d173e23929");
+const API;
+const wc = new WhaleClub("");
 //wc.markets().then(console.log);
 
 // uncomment the call you want to test
@@ -16,7 +16,7 @@ const wc = new WhaleClub("8a1c5feb-9b4f-43b8-8a2c-d7d173e23929");
    market: 'BTC-USD',
    leverage: 10,
    size: 1 * 1e8, // 1 btc
- }).then(console.log);    */
+ })
 //wc.getPosition(100).then(console.log);
 // wc.updatePosition(100, {stop_loss: 90, take_profit: 110}).then(console.log);
 // wc.closePosition("2MYtkvzGncuqj9vkk").then(console.log); 
