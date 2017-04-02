4edefdc5e7c0e0fdf5dfc9f012196ad8cc954dd3
ak9250 <ahsen.khaliq@gmail.com>
2017-04-01 20:50:01 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,32 +1,33 @@
 const WhaleClub = require('whaleclub');
 
-const API;
-const wc = new WhaleClub("");
+const API_KEY = '8a1c5feb-9b4f-43b8-8a2c-d7d173e23929';
+const wc = new WhaleClub("8a1c5feb-9b4f-43b8-8a2c-d7d173e23929");
 //wc.markets().then(console.log);
 
 // uncomment the call you want to test
 
-// wc.markets().then(console.log);
+ //wc.markets().then(console.log);
 //wc.price(['SPX500-USD']).then(console.log);
 // wc.priceTurbo(['AAPL']).then(console.log);
 // wc.balance().then(console.log);
 // wc.transactions().then(console.log);
-    wc.newPosition({
+     wc.newPosition({
    direction: 'long',
    market: 'BTC-USD',
    leverage: 10,
    size: 1 * 1e8, // 1 btc
- })
+ }).then(console.log);   
 //wc.getPosition(100).then(console.log);
 // wc.updatePosition(100, {stop_loss: 90, take_profit: 110}).then(console.log);
-// wc.closePosition("2MYtkvzGncuqj9vkk").then(console.log); 
+//wc.closePosition("2MYtkvzGncuqj9vkk").then(console.log); 
 // wc.cancelPosition(100).then(console.log);
 // wc.splitPosition(100, 20).then(console.log);
 // wc.listPositions().then(console.log);
 // wc.contracts().then(console.log);
-// wc.newTurboPosition({
-//   direction: 'long',
-//   market: 'AAPL',
-//   type: '1min',
-//   size: 1 * 1e8 // 1 btc
-// }).then(console.log);
+  /* wc.newTurboPosition({
+   direction: 'short',
+   market: 'BTC-USD',
+   type: '1min',
+   size: 1000000// 1 btc
+ }).then(console.log);   
+ */
